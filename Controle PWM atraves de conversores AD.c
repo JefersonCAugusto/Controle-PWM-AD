3d023679f92e59611ad1857ef5781d0a3b92e8c1
jeferson Augusto <carvalho.jeferson@gmail.com>
2020-04-28 22:40:47 -0300

    criei variaveis e inicializei display

--- Controle PWM atraves de conversores AD.c
+++ Controle PWM atraves de conversores AD.c
@@ -1,6 +1,24 @@
-
-//Leitura do canal A/D AN0, utilizando o canal pwn para controlar um cooler 12V e verificar a velocidade em  RPM atravez de dois censores IR
+// projeto elaborado em C(PIC) utilizando placa de desenvolvimento microgenios
+//Leitura do canal A/D AN0, utilizando o canal pwn para controlar um cooler 12V 
+//e verificar a velocidade em  RPM atravez de dois censores IR
 //usei bibliotecas de LCD 16x2,PWM,Conversor A/D e convorsoes de unidades.
+
+
+// Variáveis
+unsigned char frase[10];   //  texto.
+unsigned char porcentagem; //   PWM.
+unsigned int leituraAD = 0; //  leitura AD.
+unsigned int timer1;    // pulsos do timer 1(rpm).
+
+
+// CONFIGURAÇÃO DO  LCD.
+sbit LCD_RS at RE2_bit;
+sbit LCD_EN at RE1_bit;
+sbit LCD_D7 at RD7_bit;
+sbit LCD_D6 at RD6_bit;
+sbit LCD_D5 at RD5_bit;
+sbit LCD_D4 at RD4_bit;
+
 void main()
 
 {
