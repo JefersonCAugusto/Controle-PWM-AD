17d26c0495b1f347319059af2c28c8e46914b7b4
jeferson Augusto <carvalho.jeferson@gmail.com>
2020-04-28 23:37:19 -0300

    configurei Timers, interrupção e registradores de entrada/saída. Vou começar a lógica do programa

--- Controle PWM atraves de conversores AD.c
+++ Controle PWM atraves de conversores AD.c
@@ -22,5 +22,39 @@ sbit LCD_D4 at RD4_bit;
 void main()
 
 {
+   TRISB = 0;                        //  saida.
+   TRISD = 0;                        //  saida.
+   TRISC.RC0 = 1;                    //  pORTC.RC0 entrada.
+   TRISC.RC2 = 0;                    //  pORTC.RC2 saida.
+   TRISE = 0;                        //  saida.
+   PORTB = 0;                        //  saidas
+
+      // Configuração das interrupções
+      //geral
+   INTCON.GIEH = 1;
+   INTCON.GIEL = 1;
+   RCON.IPEN = 1;
+
+   // Timer 0
+   INTCON.TMR0IF = 0;
+   INTCON2.TMR0IP = 1;
+   INTCON.TMR0IE = 1;
+
+   T0CON = 0B10000100;
+   TMR0L = 0X7B;
+   TMR0H = 0XE1;
+   INTCON.TMR0IF = 0;
+
+   // Timer 1
+   T1CON = 0B10000011;
+   TMR1L = 0;
+   TMR1H = 0;
+   PIR1.TMR1IF = 0;
+
+   //canais A/D
+   ADCON0 = 0b00000001;
+   ADCON1 = 0b00001110;
+   ADCON2 = 0b10111110;
+
 
 }
