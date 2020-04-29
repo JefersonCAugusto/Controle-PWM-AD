4774c22f50939cdfaaca8792275e0e7aa5b62ed6
jeferson Augusto <carvalho.jeferson@gmail.com>
2020-04-28 22:32:33 -0300

    inicio do projeto

---
+++ Controle PWM atraves de conversores AD.c
@@ -0,0 +1,8 @@
+
+//Leitura do canal A/D AN0, utilizando o canal pwn para controlar um cooler 12V e verificar a velocidade em  RPM atravez de dois censores IR
+//usei bibliotecas de LCD 16x2,PWM,Conversor A/D e convorsoes de unidades.
+void main()
+
+{
+
+}
